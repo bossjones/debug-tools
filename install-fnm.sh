9e49a4fca4e7ffe42cbd37db0171515404e2a5b3
Malcolm Jones <bossjones@theblacktonystark.com>
2020-09-14 18:03:34 -0400

    feature: new debug-tools to insall asdf, pyenv, rbenv, and maybe fnm

---
+++ install-fnm.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+#-----------------------------------------------------------------------
+# Linux (Linux/x86_64, Darwin/x86_64, Linux/armv7l)
+#
+# install-rbenv.sh - Install rbenv
+#
+# usage: install-rbenv.sh
+#
+# Copyright (c) 2020 Malcolm Jones
+# All Rights Reserved.
+#-----------------------------------------------------------------------
+
+logmsg() {
+  echo ">>> $1"
+}
+
+_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+[[ -f ${_DIR}/install-config ]] && echo "This file exists! - ${_DIR}/install-config"
+
+source ${_DIR}/install-config
+
+curl -fsSL https://github.com/Schniz/fnm/raw/master/.ci/install.sh | bash
+
+# SOURCE: https://github.com/Schniz/fnm
+if [ -n "$(which fnm)" ]; then
+    eval "$(fnm env --multi)"
+    if [ "$(fnm ls | grep -i ${NODE_VERSION_TO_INSTALL} | wc -l)" = "0" ]; then
+      fnm install ${NODE_VERSION_TO_INSTALL}
+      fnm use ${NODE_VERSION_TO_INSTALL}
+      fnm default ${NODE_VERSION_TO_INSTALL}
+      fnm current
+    fi
+fi
