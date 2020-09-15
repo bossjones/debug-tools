45dff24f8541b55a4fc0d36c73740e6dfed0523d
Malcolm Jones <bossjones@theblacktonystark.com>
2020-09-15 15:59:28 -0400

    wip: compiletools

---
+++ install-compile-tools.sh
@@ -0,0 +1,16 @@
+sudo apt-get install systemtap-sdt-dev ktap -y
+
+sudo apt-get install -y bison cmake ethtool flex git iperf libstdc++6 python-netaddr python-pip gcc gcc-c++ make zlib-dev elfutils-libelf-dev gnutls-dev python-dev
+ap-get install -y clang clang-dev llvm llvm-dev
+ap-get install -y luajit luajit-dev
+pip install pyroute2
+
+sudo apt-get -y install bison build-essential cmake flex git libedit-dev \
+libllvm6.0 llvm-6.0-dev libclang-6.0-dev python zlib1g-dev libelf-dev
+
+# how to install perf cmd
+apt install linux-tools-common -y
+sudo apt-get install -y linux-headers-$(uname -r)
+apt install -y linux-cloud-tools-$(uname -r)
+
+sudo apt-get install -y linux-tools-generic linux-cloud-tools-generic
