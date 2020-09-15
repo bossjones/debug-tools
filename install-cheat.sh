ff4bd469897a4226987e5a022608c1240959bee1
Malcolm Jones <bossjones@theblacktonystark.com>
2020-09-15 15:24:47 -0400

    wip: cheat

---
+++ install-cheat.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# cheat-darwin-amd64.gz
+# 2.55 MB
+# cheat-linux-386.gz
+# 2.32 MB
+# cheat-linux-amd64.gz
+# 2.5 MB
+# cheat-linux-arm5.gz
+# 2.4 MB
+# cheat-linux-arm6.gz
+# 2.39 MB
+# cheat-linux-arm7.gz
+# 2.39 MB
+# cheat-windows-amd64.exe.zip
+# 2.56 MB
+# Source code
+# (zip)
+# Source code
+# (tar.gz)
+
+# https://github.com/cheat/cheat/releases/download/4.1.0/cheat-linux-amd64.gz
+
+# SOURCE: https://github.com/tkyonezu/Linux-tools/blob/98a373f3756fe9e27d27a8c3cf7d39fd447ea5c1/install-ctop.sh
+
+# Install cheat
+# https://github.com/cheat/cheat/releases
+
+CHEAT_VERSION=4.1.0
+
+HARDWARE=$(uname -m)
+SYSTEM=$(uname -s)
+
+if [ "${SYSTEM}" = "Linux" ]; then
+  if [ "${HARDWARE}" = "armv7l" ]; then
+    SYSTEM="linux"
+    HARDWARE="arm7"
+  elif [ "${HARDWARE}" = "armv5tejl" ]; then
+    SYSTEM="linux"
+    HARDWARE="arm5"
+  elif [ "${HARDWARE}" = "x86_64" ]; then
+    SYSTEM="linux"
+    HARDWARE="amd64"
+  else
+    echo "This platform does'nt suppot yet!!"
+    exit 1
+  fi
+elif [ "${SYSTEM}" = "Darwin" ]; then
+  if [ "${HARDWARE}" = "x86_64" ]; then
+    SYSTEM="darwin"
+    HARDWARE="amd64"
+  else
+    echo "This platform does'nt suppot yet!!"
+    exit 1
+  fi
+else
+  echo "This platform does'nt suppot yet!!"
+  exit 1
+fi
+
+# cheat-darwin-amd64.gz
+# 2.55 MB
+# cheat-linux-386.gz
+# 2.32 MB
+# cheat-linux-amd64.gz
+# 2.5 MB
+# cheat-linux-arm5.gz
+# 2.4 MB
+# cheat-linux-arm6.gz
+# 2.39 MB
+# cheat-linux-arm7.gz
+# 2.39 MB
+# cheat-windows-amd64.exe.zip
+# 2.56 MB
+# Source code
+# (zip)
+# Source code
+# (tar.gz)
+cd /usr/local/bin
+
+
+wget -c https://github.com/bcicen/ctop/releases/download/${CHEAT_VERSION}/cheat-${SYSTEM}-${HARDWARE} -O - | tar -xz -C /usr/local/bin
+
+chmod +x /usr/local/bin/cheat
+
+cd -
+
+exit 0
