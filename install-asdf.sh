d56c843bb369a8dc87a321c74a1f63b67557b24c
Malcolm Jones <bossjones@theblacktonystark.com>
2021-03-10 11:05:01 -0500

    WIP: asdf

--- install-asdf.sh
+++ install-asdf.sh
@@ -1,59 +1,27 @@
 #!/usr/bin/env bash
 
-set -e
-
-#-----------------------------------------------------------------------
-# Linux (Linux/x86_64, Darwin/x86_64, Linux/armv7l)
-#
-# install-asdf.sh - Install asdf
-#
-# usage: install-asdf.sh
-#
-# Copyright (c) 2020 Malcolm Jones
-# All Rights Reserved.
-#-----------------------------------------------------------------------
-
-logmsg() {
-  echo -e ">>> $1"
-}
-
-asdf_add_or_ignore() {
-  echo ">>> Adding asdf plugin: $1"
-  asdf plugin-add $1 $2 || echo "plugin already loaded"
-}
-
-_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-[[ -f ${_DIR}/install-config ]] && echo "This file exists! - ${_DIR}/install-config"
-source ${_DIR}/install-config
-
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0 || true
 
 echo "[asdf] enable"
 . $HOME/.asdf/asdf.sh
 
-echo " [asdf] compile dependencies"
-/usr/local/bin/install-compile-tools.sh
-
-# asdf plugin-add 1password https://github.com/samtgarson/asdf-1password.git # 1.6.0 
-# asdf plugin add goss https://github.com/raimon49/asdf-goss.git # 0.3.13 
-asdf_add_or_ignore hadolint https://github.com/looztra/asdf-hadolint # 1.18.0
-asdf_add_or_ignore fd # 8.1.1 
-asdf_add_or_ignore tmux https://github.com/aphecetche/asdf-tmux.git # 2.9a
-asdf_add_or_ignore helm https://github.com/Antiarchitect/asdf-helm.git # 3.3.1 
-asdf_add_or_ignore jsonnet https://github.com/Banno/asdf-jsonnet.git # 0.16.0
-asdf_add_or_ignore k9s https://github.com/looztra/asdf-k9s # 0.21.7 
-asdf_add_or_ignore kubectl https://github.com/Banno/asdf-kubectl.git # 1.18.6
-asdf_add_or_ignore kubectx # 0.9.1
-asdf_add_or_ignore kubeval https://github.com/stefansedich/asdf-kubeval # 0.15.0 
-asdf_add_or_ignore neovim # 0.4.4 
-asdf_add_or_ignore packer https://github.com/Banno/asdf-hashicorp.git # 1.6.2
-asdf_add_or_ignore terraform https://github.com/Banno/asdf-hashicorp.git # 0.13.2
-asdf_add_or_ignore vault https://github.com/Banno/asdf-hashicorp.git # 1.5.3
-asdf_add_or_ignore poetry https://github.com/crflynn/asdf-poetry.git # 1.0.10
-asdf_add_or_ignore yq https://github.com/sudermanjr/asdf-yq.git # 3.2.3 
-asdf_add_or_ignore ripgrep https://github.com/jgillis01/asdf-ripgrep.git
-asdf_add_or_ignore kustomize https://github.com/Banno/asdf-kustomize.git # 3.8.2
+# asdf plugin-add 1password https://github.com/samtgarson/asdf-1password.git # 1.6.0
+# asdf plugin add goss https://github.com/raimon49/asdf-goss.git # 0.3.13
+asdf plugin-add hadolint https://github.com/looztra/asdf-hadolint # 1.18.0
+asdf plugin add fd # 8.1.1
+asdf plugin-add tmux https://github.com/aphecetche/asdf-tmux.git # 2.9a
+asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git # 3.3.1
+asdf plugin-add jsonnet https://github.com/Banno/asdf-jsonnet.git # 0.16.0
+asdf plugin-add k9s https://github.com/looztra/asdf-k9s # 0.21.7
+asdf plugin-add kubectl https://github.com/Banno/asdf-kubectl.git # 1.18.6
+asdf plugin add kubectx # 0.9.1
+asdf plugin-add kubeval https://github.com/stefansedich/asdf-kubeval # 0.15.0
+asdf plugin-add neovim # 0.4.4
+asdf plugin-add packer https://github.com/Banno/asdf-hashicorp.git # 1.6.2
+asdf plugin-add terraform https://github.com/Banno/asdf-hashicorp.git # 0.13.2
+asdf plugin-add vault https://github.com/Banno/asdf-hashicorp.git # 1.5.3
+asdf plugin-add poetry https://github.com/crflynn/asdf-poetry.git # 1.0.10
+asdf plugin-add yq https://github.com/sudermanjr/asdf-yq.git # 3.2.3
 
 # asdf install goss 0.3.13
 # asdf global goss 0.3.13
@@ -79,8 +47,8 @@ asdf global kubectl 1.18.6
 asdf install kubectx 0.9.1
 asdf global kubectx 0.9.1
 
-asdf install kubeval 0.15.0 
-asdf global kubeval 0.15.0 
+asdf install kubeval 0.15.0
+asdf global kubeval 0.15.0
 
 asdf install neovim 0.4.4
 asdf global neovim 0.4.4
@@ -97,68 +65,47 @@ asdf global vault 1.5.3
 asdf install poetry 1.0.10
 asdf global poetry 1.0.10
 
-asdf install yq 3.2.3
-asdf global yq 3.2.3
+asdf install yq v4.5.0
+asdf global yq v4.5.0
 
-asdf install ripgrep 12.1.1
-asdf global ripgrep 12.1.1
-
-asdf install kustomize 3.8.2
-asdf global kustomize 3.8.2
-
-logmsg " [fd] testing"
+echo " [fd] testing"
 fd --help
 
-logmsg " [tmux] testing"
+echo " [tmux] testing"
 tmux -V
 
-logmsg " [helm] testing"
+echo " [helm] testing"
 helm --help
 
-logmsg " [helm] jsonnet"
+echo " [helm] jsonnet"
 jsonnet --help
-logmsg "\n"
 
-logmsg " [k9s] testing"
+echo " [k9s] testing"
 k9s --help
-logmsg "\n"
 
-logmsg " [kubectl] testing"
+echo " [kubectl] testing"
 kubectl version
-logmsg "\n"
 
-logmsg " [kubectx] testing"
+echo " [kubectx] testing"
 kubectx --help
-logmsg "\n"
 
-logmsg " [kubeval] testing"
+echo " [kubeval] testing"
 kubeval --version
-logmsg "\n"
 
-logmsg " [neovim] testing"
+echo " [neovim] testing"
 nvim --version
-logmsg "\n"
 
-logmsg " [packer] testing"
+echo " [packer] testing"
 packer --version
-logmsg "\n"
 
-logmsg " [terraform] testing"
+echo " [terraform] testing"
 terraform --version
-logmsg "\n"
 
-logmsg " [vault] testing"
+echo " [vault] testing"
 vault --version
-logmsg "\n"
 
-logmsg " [poetry] testing"
+echo " [poetry] testing"
 poetry --version
-logmsg "\n"
 
-logmsg " [yq] testing"
+echo " [yq] testing"
 yq --version
-logmsg "\n"
-
-logmsg " [kustomize] testing"
-kustomize version
-logmsg "\n"
