3bf2ac21caed5a9e0f98cf4e61252ff5d73a156b
Malcolm Jones <bossjones@theblacktonystark.com>
2021-09-05 20:59:54 -0400

    wip: fixing focal

--- install-compile-tools.sh
+++ install-compile-tools.sh
@@ -2,8 +2,8 @@
 sudo apt-get install systemtap-sdt-dev ktap -y
 
 sudo apt-get install -y bison cmake ethtool flex git iperf libstdc++6 python-netaddr python-pip gcc gcc-c++ make zlib-dev elfutils-libelf-dev gnutls-dev python-dev
-ap-get install -y clang clang-dev llvm llvm-dev
-ap-get install -y luajit luajit-dev
+sudo apt-get install -y clang clang-dev llvm llvm-dev
+sudo apt-get install -y luajit luajit-dev
 pip install pyroute2
 
 sudo apt-get -y install bison build-essential cmake flex git libedit-dev \
