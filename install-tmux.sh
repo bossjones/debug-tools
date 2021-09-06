45dda5262596f0b196d82f422b1e34429f15a997
Malcolm Jones <bossjones@theblacktonystark.com>
2021-09-05 21:45:13 -0400

    wip: fixing focal

--- install-tmux.sh
+++ install-tmux.sh
@@ -27,7 +27,7 @@ _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # source ${_DIR}/install-config
 
 mkdir -p ~/dev/bossjones || true
-git clone git@github.com:bossjones/.tmux.git ~/dev/bossjones/oh-my-tmux || true
+git clone https://github.com/bossjones/.tmux.git ~/dev/bossjones/oh-my-tmux || true
 ln -v -s -f ~/dev/bossjones/oh-my-tmux/.tmux.conf ~/.tmux.conf || true
 cp -av ~/dev/bossjones/oh-my-tmux/.tmux.conf.local ~/.tmux.conf.local || true
 
