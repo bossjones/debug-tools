ccacda09c574f20d4f7e578a9129daa947ddb623
Malcolm Jones <bossjones@theblacktonystark.com>
2021-09-05 22:47:53 -0400

    wip: fixing focal

--- install-node.sh
+++ install-node.sh
@@ -22,6 +22,12 @@ logmsg ">>> Install Node.js 8.x"
 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
-sudo apt install -y node.js
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn -y
+
+
+sudo apt-get install gcc g++ make -y
+sudo apt-get install -y nodejs
 
 exit 0
