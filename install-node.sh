c3848b7d5a2c52cae9c2dea562bc0991b5bb98bb
Malcolm Jones <bossjones@theblacktonystark.com>
2021-09-05 22:46:39 -0400

    wip: fixing focal

--- install-node.sh
+++ install-node.sh
@@ -20,7 +20,7 @@ logmsg() {
 #
 logmsg ">>> Install Node.js 8.x"
 
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
 sudo apt install -y node.js
 
