89cb7c29e24ed3f2de0d567a463957d36c055aa9
Malcolm Jones <bossjones@theblacktonystark.com>
2022-01-08 12:51:58 -0500

    Feature: fpp

---
+++ install-fpp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -x
+
+#-----------------------------------------------------------------------
+# Linux (Linux/x86_64, Darwin/x86_64, Linux/armv7l)
+#
+# install-fpp.sh - Install Facebook path picker
+#
+# usage: install-fpp.sh
+#
+# Copyright (c) 2022 bossjones
+# All Rights Reserved.
+#-----------------------------------------------------------------------
+
+logmsg() {
+  echo ">>> $1"
+}
+
+
+logmsg ">>> Install fpp ${VERSION}"
+cd /var/tmp
+git clone https://github.com/facebook/PathPicker.git || true
+cd PathPicker/debian
+./package.sh
+ls ../fpp_0.7.2_noarch.deb
+pwd
+cd -
+
+exit 0
