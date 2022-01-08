077a683a9bdc4ada77662fc23be14d18330c88f5
Malcolm Jones <bossjones@theblacktonystark.com>
2022-01-08 15:53:29 -0500

    Feature: 20.04 update zsh

--- install-docker2.sh
+++ install-docker2.sh
@@ -41,6 +41,9 @@ sh get-docker.sh
 sudo usermod -aG docker $NON_ROOT_USER
 # apt-get install -y apt-transport-https curl
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 
 
 exit 0
---
+++ install-peco.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+#-----------------------------------------------------------------------
+# Linux (Linux/x86_64, Darwin/x86_64, Linux/armv7l)
+#
+# install-rbenv.sh - Install rbenv
+#
+# usage: install-rbenv.sh
+#
+# Copyright (c) 2020 Malcolm Jones
+# All Rights Reserved.
+#-----------------------------------------------------------------------
+
+logmsg() {
+  echo ">>> $1"
+}
+
+_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+[[ -f ${_DIR}/install-config ]] && echo "This file exists! - ${_DIR}/install-config"
+
+source ${_DIR}/install-config
+
+apt install peco -y
