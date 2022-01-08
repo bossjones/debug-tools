da466103dded2477a013446c206847fb119d2ea2
Malcolm Jones <bossjones@theblacktonystark.com>
2022-01-08 14:16:46 -0500

    Feature: 20.04 update zsh

--- install-docker2.sh
+++ install-docker2.sh
@@ -6,8 +6,8 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 # # run docker commands as vagrant user (sudo not required)
-usermod -aG docker `whoami`
-apt-get install -y apt-transport-https curl
+sudo usermod -aG docker `whoami`
+# apt-get install -y apt-transport-https curl
 
 
 
